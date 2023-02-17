58ed1437ac91686f112dc8641c6081dd411dc9a2
bendevio <>
2023-02-17 18:59:41 +0000

    added github corner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Footer from "./components/Footer";
+import GithubCorner from 'react-github-corner';
 import Resume from "./components/Resume/Resume";
 import {
 	BrowserRouter as Router,
@@ -18,7 +19,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Skills from "./components/Skills/Skills";
 import Courses from "./components/Courses/Courses";
 import Articles from "./components/Articles/Articles";
-
+        
 function App() {
 	const [load, upadateLoad] = useState(true);
 
@@ -35,6 +36,7 @@ function App() {
 			<Preloader load={load} />
 			<div className="App" id={load ? "no-scroll" : "scroll"}>
 				<Navbar />
+        <GithubCorner href="https://github.com/ben9992/my_portfolio_react" />
 				<ScrollToTop />
 				<Routes>
 					<Route path="/" element={<Home />} />
