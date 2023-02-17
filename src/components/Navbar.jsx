ebbaaa0f6345446acf0db519b0cf2b709ef06b41
Ben Mishali <ben9992@gmail.com>
2023-02-17 19:13:26 +0000

    Update App.jsx and Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,110 +3,113 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-import {
-	AiOutlineHome,
-	AiOutlineUser,
-} from "react-icons/ai";
-import { BsBook} from "react-icons/bs";
+import GithubCorner from "react-github-corner";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BsBook } from "react-icons/bs";
 import { FaBrain } from "react-icons/fa";
 import { GrArticle } from "react-icons/gr";
 
 import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
-	const [expand, updateExpanded] = useState(false);
-	const [navColour, updateNavbar] = useState(false);
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
 
-	function scrollHandler() {
-		if (window.scrollY >= 20) {
-			updateNavbar(true);
-		} else {
-			updateNavbar(false);
-		}
-	}
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
 
-	window.addEventListener("scroll", scrollHandler);
+  window.addEventListener("scroll", scrollHandler);
 
-	return (
-		<Navbar
-			expanded={expand}
-			fixed="top"
-			expand="md"
-			className={navColour ? "sticky" : "navbar"}
-		>
-			<Container>
-				<Navbar.Toggle
-					aria-controls="responsive-navbar-nav"
-					onClick={() => {
-						updateExpanded(expand ? false : "expanded");
-					}}
-				>
-					<span></span>
-					<span></span>
-					<span></span>
-				</Navbar.Toggle>
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav defaultActiveKey="#home">
-						<Nav.Item>
-							<Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-								<AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-							</Nav.Link>
-						</Nav.Item>
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <GithubCorner href="https://github.com/ben9992/my_portfolio_react" />
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/about"
-								onClick={() => updateExpanded(false)}
-							>
-								<AiOutlineUser style={{ marginBottom: "2px" }} /> About
-							</Nav.Link>
-						</Nav.Item>
+      <Container>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/skills"
-								onClick={() => updateExpanded(false)}
-							>
-								<FaBrain style={{ marginBottom: "2px" }} /> Skills
-							</Nav.Link>
-						</Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/courses"
-								onClick={() => updateExpanded(false)}
-							>
-								<BsBook style={{ marginBottom: "2px" }} /> Courses
-							</Nav.Link>
-						</Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/skills"
+                onClick={() => updateExpanded(false)}
+              >
+                <FaBrain style={{ marginBottom: "2px" }} /> Skills
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/articles"
-								onClick={() => updateExpanded(false)}
-							>
-								<GrArticle style={{ marginBottom: "2px", fontColor:"white" }} /> Articles
-							</Nav.Link>
-						</Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/courses"
+                onClick={() => updateExpanded(false)}
+              >
+                <BsBook style={{ marginBottom: "2px" }} /> Courses
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/resume"
-								onClick={() => updateExpanded(false)}
-							>
-								<CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-							</Nav.Link>
-						</Nav.Item>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/articles"
+                onClick={() => updateExpanded(false)}
+              >
+                <GrArticle
+                  style={{ marginBottom: "2px", fontColor: "white" }}
+                />{" "}
+                Articles
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
